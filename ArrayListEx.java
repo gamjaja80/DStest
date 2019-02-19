9616094077bf3aa3eb84c6eb969aa5acbe75ae35
gamjaja80 <45969109+gamjaja80@users.noreply.github.com>
2019-02-19 23:35:44 +0900

    Add files via upload

---
+++ ArrayListEx.java
@@ -0,0 +1,20 @@
+package review;
+
+import java.util.ArrayList;
+
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+		
+		String[] arr = new String[2];
+		
+		arr[0] = "사과";
+		arr[1] = "포도";
+		
+		ArrayList<String> list = new ArrayList<>();
+		list.add("복숭아");
+		list.add("바나나");
+		
+	}
+
+}
