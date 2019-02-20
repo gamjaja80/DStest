eca9ae25a81b5a019c266122d313e1c25a3a812b
gamjaja80 <45969109+gamjaja80@users.noreply.github.com>
2019-02-21 00:11:29 +0900

    Add files via upload

--- ArrayEx.java
+++ ArrayEx.java
@@ -1,6 +1,9 @@
 package review;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class ArrayEx {
@@ -20,8 +23,32 @@ public class ArrayEx {
 			
 			List<Integer> sList = list.subList(fidx, eidx+1);
 			System.out.println(sList);
+			
+			ArrayList<Integer> list2 = new ArrayList<>(sList);//리스트 복사
+			
+		}else {
+			System.out.println("20과 40이 없습니다.");
 		}
 		
+		Collections.shuffle(list);
+		System.out.println("shuffle : "+list);
+		
+		Collections.reverse(list);
+		System.out.println("reverse : " + list);
+		
+		Collections.sort(list);
+		System.out.println("오름차순 정렬 : " + list);
+		Collections.reverse(list);
+		System.out.println("내림차순 정렬 : " + list);
+		
+		list.remove(new Integer(40)); //숫자를 그냥쓰면 오브젝트아 충돌
+		System.out.println(list);
+		
+		for(int i = list.size()-1; i>=0 ; i--) {
+			if(list.get(i) >= 30) {
+				list.remove(i);
+			}
+		}System.out.println(list);
 	}
 
 }
